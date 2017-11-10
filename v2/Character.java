7574d861d53da688cf11dd9d5f8aca34ed9ec710
Kevin Lin <33496321+Stuy-Kevin-Lin@users.noreply.github.com>
2017-11-10 13:44:42 -0500

    Added heading

--- v2/Character.java
+++ v2/Character.java
@@ -1,3 +1,10 @@
+/*
+Team Rollas
+Kevin Lin, Shruthi, John
+APCS pd8
+HW30 -- Ye Olde Role Playing Game, Improved
+2017-11-10
+*/
 public class Character {
     //instance vars
     public int health;
